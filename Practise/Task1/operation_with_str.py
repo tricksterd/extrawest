b3411f2e51396f45e3e9b1a9fbc788072d87c7ed
viktor-198 <vinitskiyv@gmail.com>
2021-12-04 19:39:56 +0200

    Example. Using string methods

--- Practise/Task1/operation_with_str.py
+++ Practise/Task1/operation_with_str.py
@@ -4,3 +4,7 @@ temp_str = temp_str.replace('R', 'Л')
 temp_str = temp_str.split(' ')[0].capitalize() + ' ' + temp_str.split(' ')[1].capitalize() \
            + ' ' + temp_str.split(' ')[2].capitalize()
 print(temp_str)
+
+
+full_name = " козявко Rеонід DdD"
+print(full_name.strip().lower().replace('r', 'Л').replace('ddd', 'Олександрович').title())
