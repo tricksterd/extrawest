7398307ae475679a4724825dc068d5f5eb9fa19d
tricksterd <kozavkolena@gmail.com>
2021-12-20 22:16:29 +0200

    Added Practise/Task4 (Debug sockets)

---
+++ Practise/Task4/socket_server.py
@@ -0,0 +1,44 @@
+import sys
+import json
+import socket
+
+server_socket = socket.socket()
+
+print("Server socket created")
+
+ip = "127.0.0.1"
+
+port = 35491
+
+server_socket.bind((ip, port))
+
+print("Server socket bound with with ip {} port {}".format(ip, port))
+
+server_socket.listen()
+
+count = 0
+
+while True:
+    try:
+        client_connection, client_address = server_socket.accept()
+        count += 1
+        data_arr = []
+        print(f"Accepted {count} {client_address} connections so far")
+        while True:
+            data = client_connection.recv(8192)
+            data_arr.append(data)
+            if data != b'':
+                for i in data_arr:
+                    print(json.loads(i.decode()))
+                msg1 = "Hi Client! Read everything you sent".encode()
+                msg2 = "Now I will close your connection".encode()
+                client_connection.send(msg1)
+                client_connection.send(msg2)
+                client_connection.close()
+                print("Connection closed")
+                break
+
+    except KeyboardInterrupt:
+        print('Server socket close')
+        server_socket.close()
+        sys.exit()
